719b5cc0419fec3d315f1853087d4b03b2a79a63
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2022-04-05 00:15:35 +0530

    quiz trail

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData=[
+    {
+        question:'How old is Akanksha?',
+        a:'10',
+        b:'22',
+        c:'24',
+        d:'20',
+        correct:'b'
+    },{
+        question:'Which is the most used programming language in 2022?',
+        a:'Javascript',
+        b:'Cpp',
+        c:'Java',
+        d:'Python',
+        correct:'d'
+    },{
+        question:'Who is the Prime Minister of India?',
+        a:'Narendra Modi',
+        b:'Amit Shah',
+        c:'Manmohan Singh',
+        d:'Sonia Gandhi',
+        correct:'a'
+    },{
+        question:'What does HTML stands for?',
+        a:'Hyper Text Maintanence Language',
+        b:'Hyper Transfer Markup Language',
+        c:'Hyper Text Markup Language',
+        d:'Hyper Text Mark Library',
+        correct:'b'
+    },{question:'When was javascript launched?',
+    a:'2020',
+    b:'2019',
+    c:'2017',
+    d:'None of the above',
+    correct:'d'
+        
+    }
+]
