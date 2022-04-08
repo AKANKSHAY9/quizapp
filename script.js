15e7c03f359cb7e35d70d05ad105493fafde538a
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2022-04-08 23:11:25 +0530

    quiz

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const quizData=[
     {
-        question:'How old is Akanksha?',
-        a:'10',
-        b:'22',
-        c:'24',
-        d:'20',
+        question:'When was Google founded?',
+        a:'2010',
+        b:'1998',
+        c:'1999',
+        d:'2000',
         correct:'b'
     },{
         question:'Which is the most used programming language in 2022?',
@@ -36,3 +36,29 @@ const quizData=[
         
     }
 ]
+const questionE1= document.getElementById('question');
+console.log(questionE1);
+const a_text =document.getElementById('a_text');
+const b_text =document.getElementById('b_text');
+const c_text =document.getElementById('c_text');
+const d_text =document.getElementById('d_text');
+let currentQuiz=0;
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+    console.log(" helllo",currentQuizData);
+    questionE1.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+    currentQuiz++;
+}
+
+const submit=document.getElementById('submit');
+submit.addEventListener('click',()=>{
+    
+
+})
